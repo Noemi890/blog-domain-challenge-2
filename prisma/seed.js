a47a0e18c8d61435a98c184031f994e5f2cc07a2
Noemi <noemi.caggiano@gmail.com>
2022-09-02 17:38:25 +0100

    updated schema w/cascade and seed for more records

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,7 +11,15 @@ async function seed() {
     }
   })
 
-  console.log('user', createdUser)
+  const secondUser = await prisma.user.create({
+    data: {
+      username: "Blah",
+      email: "blah@blah",
+      password: "djsalkdjsa"
+    }
+  })
+
+  console.log('user', createdUser, secondUser)
 
   const createdProfile = await prisma.profile.create({
     data: {
@@ -23,7 +31,17 @@ async function seed() {
     }
   })
 
-  console.log('profile', createdProfile)
+  const secondProfile = await prisma.profile.create({
+    data: {
+      userId: secondUser.id,
+      firstName: "Blah",
+      lastName: "Blah",
+      age: 30,
+      pictureUrl: "some img"
+    }
+  })
+
+  console.log('profile', createdProfile, secondProfile)
 
   const createdPost = await prisma.post.create({
     data: {
@@ -31,6 +49,15 @@ async function seed() {
       content: "I'm losing my patience creating a DB",
       imageUrl: "https://images.unsplash.com/photo-1527631120902-378417754324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
       userId: createdUser.id
+    }
+  })
+
+  const secondPost = await prisma.post.create({
+    data: {
+      title: "I'm exausted",
+      content: "Today I'm feeling meh",
+      imageUrl: "some img",
+      userId: secondUser.id
     }
   })
 
@@ -42,7 +69,7 @@ async function seed() {
       replies: {
         create: [
           {
-            userId: createdUser.id,
+            userId: secondUser.id,
             postId: createdPost.id,
             content: "You're crazy"
           }
@@ -54,7 +81,32 @@ async function seed() {
     }
   })
 
-  console.log('comment', createdComment, createdPost)
+  const secondComment = await prisma.comment.create({
+    data: {
+      userId: secondUser.id,
+      postId: secondPost.id,
+      content: "Tell me about it",
+      replies: {
+        create: [
+          {
+            userId: createdUser.id,
+            postId: secondPost.id,
+            content: "I'm having a wonderful time!"
+          },
+          {
+            userId: secondUser.id,
+            postId: secondPost.id,
+            content: "You can't talk, you're not real!"
+          }
+        ]
+      }
+    },
+    include: {
+      replies: true
+    }
+  })
+
+  console.log('comment', createdComment, createdPost, secondPost, secondComment)
 
   const createdCategory = await prisma.category.create({
     data: {
